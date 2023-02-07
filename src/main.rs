2023f0b41b18811c8e8758c2a1a67ecbe82d0838
lucas-mior <lucas.mior@tutamail.com>
2023-02-07 14:49:08 -0300

    use macro for usage

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ use std::cmp;
 static NUMBERS: &'static [i32] = &[0, 10, 20, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 93, 96, 100];
 static DEF_OPACITY: usize = 13;
 
+static USAGE: &str = "rurxvt_opacity [-+=h]
+- -- decrease
++ -- increase
+= -- set 100% opaque
+h -- print this help message";
+macro_rules! usage {
+    () => { 
+        println!("{}", USAGE);
+    };
+}
+
 fn main() {
     let argv: Vec<String> = env::args().collect();
     const NAME: &str = "opacity";
@@ -15,7 +26,7 @@ fn main() {
     let window_id: i32;
 
     if argv.len() <= 1 {
-        help();
+        usage!();
         exit(1);
     } else {
         window_id = getppid().into();
@@ -37,7 +48,7 @@ fn main() {
         "+" => current = cmp::min(current + 1, NUMBERS.len() - 1),
         "=" => current = NUMBERS.len() - 1,
         _ => { 
-            help(); 
+            usage!(); 
             exit(1);
         }
     };
@@ -47,12 +58,4 @@ fn main() {
     print!("\x1b]708;[{}]#000000\x07", NUMBERS[current]); //border
 
     return;
-}
-
-fn help() {
-    println!("rurxvt_opacity [-+=h]\n
-             - -- decrease\n
-             + -- increase\n
-             = -- set 100% opaque\n
-             h -- print this help message");
 }
